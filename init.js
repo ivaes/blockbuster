729c90c2e4effe5b47dffa0d86ef6acdadbaeb1f
asafonov <me@asafonov.org>
2020-04-29 17:17:11 +0300

    added start button

--- init.js
+++ init.js
@@ -2,9 +2,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var view = new FieldView();
   view.field = new Field();
   view.init();
-  var ball = new Ball();
-  view.field.setBall(ball);
-  view.field.setHero(new Subject());
-  var levels = new Levels(view.field);
-  view.field.setObjectMap(levels.getRandom());
+  window.view = view;
+  document.querySelector('#start button').focus();
 });
+
+function start() {
+  document.getElementById('start').style.display = 'none';
+  (new Audio('sound/explosion.mp3')).play();
+  var ball = new Ball();
+  window.view.field.setBall(ball);
+  window.view.field.setHero(new Subject());
+  var levels = new Levels(window.view.field);
+  window.view.field.setObjectMap(levels.getRandom());
+}
